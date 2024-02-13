2ad5d90fabef196ed63d7151177fe6589556caab
Naturalenemy07 <john.d.caruthers@gmail.com>
2024-02-13 09:24:43 -0500

    added instructions for cust lib

--- vscode/hroml/test/test.py
+++ vscode/hroml/test/test.py
@@ -1,5 +1,7 @@
 from hroml import test
 
-def test_hroml():
-    test
-    return
+def ex21(input_x):
+    if input_x == 0:
+        raise ZeroDivisionError
+    else:
+        return (1/input_x + 3*input_x)
