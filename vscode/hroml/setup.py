38bc4fa0528da58a0e5be2e31d78782cdd483ce2
Naturalenemy07 <john.d.caruthers@gmail.com>
2024-02-13 09:53:05 -0500

    updated to v 0.1.2

--- vscode/hroml/setup.py
+++ vscode/hroml/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='hroml',
     packages=find_packages(include=['roml']),
-    version='0.1.1',
+    version='0.1.2',
     description='My first Python library',
     author='Me',
     install_requires=[]
