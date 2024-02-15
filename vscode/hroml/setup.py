77c3500ab204e4179b71b72f2086a95d02f2e54d
Naturalenemy07 <john.d.caruthers@gmail.com>
2024-02-14 20:03:16 -0500

    updated v 0.1.11

--- vscode/hroml/setup.py
+++ vscode/hroml/setup.py
@@ -3,11 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='hroml',
     packages=find_packages(include=['hroml']),
-    version='0.1.10',
+    version='0.1.11',
     description='Python library for Hood College, CS543 Machine learning class',
     author='John Caruthers',
     install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.1.0'],
-    test_suite='tests'
 )
