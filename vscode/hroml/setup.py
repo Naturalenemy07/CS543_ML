acaf753428d18a0eadddae700c9be196da21e14b
Naturalenemy07 <john.d.caruthers@gmail.com>
2024-02-24 12:40:03 -0500

    updated hroml 0.2.0

--- vscode/hroml/setup.py
+++ vscode/hroml/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='hroml',
     packages=find_packages(include=['hroml']),
-    version='0.1.13',
+    version='0.2.0',
     description='Python library for Hood College, CS543 Machine learning class',
     author='John Caruthers',
-    install_requires=[],
+    install_requires=['numpy', 'pandas'],
 )
