c332c1d6fc734d2f3c3a7217f2f03ecd1b79e6b6
Naturalenemy07 <john.d.caruthers@gmail.com>
2024-02-25 11:49:26 -0500

    updated 0.2.6

--- vscode/hroml/myfunctions/mlfuncs.py
+++ vscode/hroml/myfunctions/mlfuncs.py
@@ -12,15 +12,9 @@ errIdx = "Upper bound index must be greater than lower bound index."
 """""""""
 Functions
 """""""""
-def __mydot(xi, w):
-    n = len(w)
-    result = 0.0
-    for j in range(n):
-        result += xi[j]*w[j]
-    return result
 
 def lin_regress(bias, w, xi):
-    return bias + __mydot(xi, w)
+    return bias + xi.dot(w)
 
 def train_linear_regression(X, y):
     ones = np.ones(X.shape[0])
@@ -64,3 +58,56 @@ def rsme(y, y_pred):
     error = y_pred - y
     mse = (error**2).mean()
     return np.sqrt(mse)
+
+def MLB_ch2_prepare_X(input_df, base_columns):
+    
+    input_df = input_df.copy()
+    features = base_columns.copy()
+
+    input_df['age'] = 2017 - input_df.year
+    features.append('age')
+
+    for v in [2,3,4]:
+        feature = 'num_doors_%s' % v
+        input_df[feature] = (input_df['number_of_doors'] == v).astype(int)
+        features.append(feature)
+
+    for v in ['chevrolet','ford','volkswagon','toyota','dodge']:
+        feature = 'is_make_%s' % v
+        input_df[feature] = (input_df['make'] == v).astype(int)
+        features.append(feature)
+    
+    for v in ['regular_unleaded','premium_unleaded_(required)','premium_unleaded_(recommended)','flex-fuel_(unleaded/e85)']:
+        feature = 'is_type_%s' % v
+        input_df[feature] = (input_df['engine_fuel_type'] == v).astype(int)
+        features.append(feature)
+
+    for v in ['automatic','manual','automated_manual']:
+        feature = 'is_transmission_%s' % v
+        input_df[feature] = (input_df['transmission_type'] == v).astype(int)
+        features.append(feature)
+    
+    for v in ['front_wheel_drive','rear_wheel_drive','all_wheel_drive','four_wheel_drive']:
+        feature = 'is_driven_wheels_%s' % v
+        input_df[feature] = (input_df['driven_wheels'] == v).astype(int)
+        features.append(feature)
+
+    for v in ['crossover','flex_fuel','luxury','luxury_performance','hatchback']:
+        feature = 'is_mc_%s' % v
+        input_df[feature] = (input_df['market_category'] == v).astype(int)
+        features.append(feature)
+    
+    for v in ['compact','midsize','large']:
+        feature = 'is_size_%s' % v
+        input_df[feature] = (input_df['vehicle_size'] == v).astype(int)
+        features.append(feature)
+
+    for v in ['sedan','4dr_suv','coupe','convertible','4dr_hatchback']:
+        feature = 'is_style_%s' % v
+        input_df[feature] = (input_df['vehicle_style'] == v).astype(int)
+        features.append(feature)
+    
+    df_num_prep = input_df[features]
+    df_num_prep = df_num_prep.fillna(0)
+    X_prep = df_num_prep.values
+    return X_prep
